21fb1607896b7e7737efa099b14e929f0e9204db
IanNicholson10 <98067643+IanNicholson10@users.noreply.github.com>
2025-03-19 11:27:35 -0600

    Added models for Park, LotType, and Lot. Added index and upsert pages for LotTypes and Parks

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -9,6 +9,5 @@ public class ApplicationDbContext : IdentityDbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
-         public DbSet<FILLER> FILLER { get; set; }
 }
 }
