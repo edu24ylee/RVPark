342953d2a09fed01ebdd56c197caee49eaf3b157
TawnyMcAleese <tawnymcaleese@mail.weber.edu>
2025-03-21 20:29:38 -0600

    Reservation Pages/controller

--- RVPark/Pages/Admin/Reservations/Index.cshtml.cs
+++ RVPark/Pages/Admin/Reservations/Index.cshtml.cs
@@ -1,12 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ApplicationCore.Models;
+using Infrastructure.Data;
 
 namespace RVPark.Pages.Admin.Reservations
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly UnitOfWork _unitOfWork;
+
+        public IndexModel(UnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+
+        public List<Reservation> Reservations { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            Reservations = (List<Reservation>)await _unitOfWork.Reservation.GetAllAsync(
+                includes: "Guest.User,Lot,Rv"
+            );
         }
     }
 }
