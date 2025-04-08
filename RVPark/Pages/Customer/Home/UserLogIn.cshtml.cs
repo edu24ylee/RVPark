3c5866b59927c68f07507d355034808576fbbf53
LexusLomaLindeman <LexusLindeman@gmail.com>
2025-04-07 23:05:16 -0600

    Finished a dummy schedule, payment log in create account, and user profile so they can edit and cancel the reservation.

---
+++ RVPark/Pages/Customer/Home/UserLogIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RVPark.Pages.Customer.Home
+{
+    public class UserLogInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
